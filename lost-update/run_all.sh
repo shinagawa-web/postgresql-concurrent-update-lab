fb89fe2ac396cf5ce81e1e22d198c95ad418a789
Kazutomo Deguchi <kazutomo.deguchi@shinagawa-web.com>
2026-09-04 08:55:39 +0900

    Fix schema.sql path in lost-update/run_all.sh and rename workflow throughput -> hold-time

--- lost-update/run_all.sh
+++ lost-update/run_all.sh
@@ -3,7 +3,7 @@ set -e
 
 docker compose up -d
 docker compose exec -T postgres sh -c 'until pg_isready -U postgres; do sleep 1; done'
-docker compose exec -T postgres psql -U postgres -d lab -f /dev/stdin < schema.sql
+docker compose exec -T postgres psql -U postgres -d lab -f /dev/stdin < lost-update/schema.sql
 
 run() {
   echo "=== $* ==="
